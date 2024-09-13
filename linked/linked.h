f84fe4ee49cbcdad5dd76f566d448f1f19d3718c
aceinetx <mouselin82@gmail.com>
2024-09-13 20:17:19 +0300

    add operators & format tests.cpp and linked.h

--- linked/linked.h
+++ linked/linked.h
@@ -1,11 +1,11 @@
 /*
-*
-* Linked:
-*   A double-linked array library
-*
-* Aceinet (2022-2024)
-*
-*/
+ *
+ * Linked:
+ *   A double-linked array library
+ *
+ * Aceinet (2022-2024)
+ *
+ */
 
 #ifndef _LINKED_H_
 #define _LINKED_H_
@@ -16,152 +16,203 @@
 #include <stdlib.h>
 #include <cstring>
 
-namespace aceinetzxx {
+namespace aceinetzxx
+{
 
-  class LinkedError : public std::exception {
-    public:
-      std::string message;
+  class LinkedError : public std::exception
+  {
+  public:
+    std::string message;
 
-      char *what(){
-        return (char*)message.c_str();
-      }
+    char *what()
+    {
+      return (char *)message.c_str();
+    }
 
-      LinkedError(std::string x){
-        message = x;
-      }
+    LinkedError(std::string x)
+    {
+      message = x;
+    }
   };
 
-  struct linked_entry {
-		char* element_bytes;
-		linked_entry* next;
-		linked_entry* previous;
-	};
+  struct linked_entry
+  {
+    char *element_bytes;
+    linked_entry *next;
+    linked_entry *previous;
+  };
 
-	template<class T>
-	class linked {
-    public:
-		  size_t element_size;
-		  linked_entry* begin;
+  template <class T>
+  class linked
+  {
+  public:
+    size_t element_size;
+    linked_entry *begin;
 
-    public:
-      // (de)constructors
-      linked(){
-        this->element_size = sizeof(T);
-        this->begin = nullptr;
+  public:
+    // (de)constructors
+    linked()
+    {
+      this->element_size = sizeof(T);
+      this->begin = nullptr;
+    }
+
+    ~linked()
+    {
+      linked_entry *current_entry = this->begin;
+
+      while (true)
+      {
+        if (current_entry == NULL)
+          return;
+
+        linked_entry *old_entry = current_entry;
+        current_entry = current_entry->next;
+        delete old_entry->element_bytes;
+        delete old_entry;
       }
+    }
 
-      ~linked(){
-        linked_entry *current_entry = this->begin;
+    // operators
+    T &operator[](size_t i)
+    {
+      return at(i);
+    }
 
-        while(true){
-          if(current_entry == NULL)
-            return;
+    aceinetzxx::linked<T> operator*(int times)
+    {
+      aceinetzxx::linked<T> result;
 
-          linked_entry *old_entry = current_entry;
-          current_entry = current_entry->next;
-          delete old_entry->element_bytes;
-          delete old_entry;
+      for (int i = 0; i < times; i++)
+      {
+        for (int j = 0; j < size(); j++)
+        {
+          result.push_back(this->operator[](j));
         }
       }
 
-      // operators
-      T& operator[](size_t i){
-        return at(i);
-      }
+      return result;
+    }
 
+    void operator*=(int times)
+    {
+      size_t old_size = size();
 
-      // functions
-      void push_back(T arg){
-        if(this->begin == nullptr){
-          this->begin = new aceinetzxx::linked_entry();
-          this->begin->element_bytes = new char[this->element_size];
-          memcpy(this->begin->element_bytes, &arg, this->element_size);
-          this->begin->next = nullptr;
-          this->begin->previous = nullptr;
-        } else {
-          linked_entry *current_entry = this->begin;
-          while(true){
-            if(current_entry->next == nullptr) break;
-            current_entry = current_entry->next;
-          }
-          current_entry->next = new aceinetzxx::linked_entry();
-          current_entry->next->next = nullptr;
-          current_entry->next->element_bytes = new char[this->element_size];
-          memcpy(current_entry->next->element_bytes, &arg, this->element_size);
-          current_entry->next->previous = current_entry;
+      for (int i = 1; i < times; i++)
+      {
+        for (int j = 0; j < old_size; j++)
+        {
+          push_back(this->operator[](j));
         }
       }
+    }
 
-      T pop_back(){
+    // functions
+    void push_back(T arg)
+    {
+      if (this->begin == nullptr)
+      {
+        this->begin = new aceinetzxx::linked_entry();
+        this->begin->element_bytes = new char[this->element_size];
+        memcpy(this->begin->element_bytes, &arg, this->element_size);
+        this->begin->next = nullptr;
+        this->begin->previous = nullptr;
+      }
+      else
+      {
         linked_entry *current_entry = this->begin;
-        while(true){
+        while (true)
+        {
           if (current_entry->next == nullptr)
-          {
-            T element = *(int *)current_entry->element_bytes;
-            current_entry->previous->next = nullptr;
-            delete current_entry->element_bytes;
-            delete current_entry;
-            return element;
-          }
-          else
-          {
-            current_entry = current_entry->next;
-          }
-        }
-      }
-
-      T& back(){
-        linked_entry *current_entry = this->begin;
-        while(true){
-          if (current_entry->next == nullptr)
-          {
-            return *(int*)current_entry->element_bytes; 
-          }
-          else
-          {
-            current_entry = current_entry->next;
-          }
-        }
-        throw aceinetzxx::LinkedError("back() failed");
-      }
-
-      T& at(size_t idx){
-        linked_entry *current_entry = this->begin;
-        size_t pos=0;
-        while(true){
-          if(pos == idx)
-          {
-            //T element;
-            //memcpy(&element, current_entry->element_bytes, this->element_size);
-            //return element;
-            return *(int*)current_entry->element_bytes;
-          }
-
-          if(current_entry->next == nullptr)
-          {
-            throw aceinetzxx::LinkedError("at() failed");
-          }
-
-          current_entry = current_entry->next;
-          pos++;
-        }
-      }
-
-      size_t size(){
-        size_t result = 0;
-
-        linked_entry *current_entry = this->begin;
-        while(true){
-          result++;
-
-          if(current_entry->next == nullptr)
             break;
-
           current_entry = current_entry->next;
         }
-
-        return result;
+        current_entry->next = new aceinetzxx::linked_entry();
+        current_entry->next->next = nullptr;
+        current_entry->next->element_bytes = new char[this->element_size];
+        memcpy(current_entry->next->element_bytes, &arg, this->element_size);
+        current_entry->next->previous = current_entry;
       }
+    }
+
+    T pop_back()
+    {
+      linked_entry *current_entry = this->begin;
+      while (true)
+      {
+        if (current_entry->next == nullptr)
+        {
+          T element = *(int *)current_entry->element_bytes;
+          current_entry->previous->next = nullptr;
+          delete current_entry->element_bytes;
+          delete current_entry;
+          return element;
+        }
+        else
+        {
+          current_entry = current_entry->next;
+        }
+      }
+    }
+
+    T &back()
+    {
+      linked_entry *current_entry = this->begin;
+      while (true)
+      {
+        if (current_entry->next == nullptr)
+        {
+          return *(int *)current_entry->element_bytes;
+        }
+        else
+        {
+          current_entry = current_entry->next;
+        }
+      }
+      throw aceinetzxx::LinkedError("back() failed");
+    }
+
+    T &at(size_t idx)
+    {
+      linked_entry *current_entry = this->begin;
+      size_t pos = 0;
+      while (true)
+      {
+        if (pos == idx)
+        {
+          // T element;
+          // memcpy(&element, current_entry->element_bytes, this->element_size);
+          // return element;
+          return *(int *)current_entry->element_bytes;
+        }
+
+        if (current_entry->next == nullptr)
+        {
+          throw aceinetzxx::LinkedError("at() failed");
+        }
+
+        current_entry = current_entry->next;
+        pos++;
+      }
+    }
+
+    size_t size()
+    {
+      size_t result = 0;
+
+      linked_entry *current_entry = this->begin;
+      while (true)
+      {
+        result++;
+
+        if (current_entry->next == nullptr)
+          break;
+
+        current_entry = current_entry->next;
+      }
+
+      return result;
+    }
   };
 }
 
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -2,7 +2,8 @@
 #include <iostream>
 #include <vector>
 
-int main(int argc, char** argv){
+int main(int argc, char **argv)
+{
 
   int num = 123;
   int sixty_nine = 69;
@@ -15,51 +16,72 @@ int main(int argc, char** argv){
   int failed_count = 0;
 
   auto element = x.back();
-  if(element == num){
+  if (element == num)
+  {
     std::cout << "[1] (back) Passed" << std::endl;
-  } else {
+  }
+  else
+  {
     std::cout << "[1] (back) Failed" << std::endl;
     failed_count++;
   }
 
-  if(x.size() == 3){
+  if (x.size() == 3)
+  {
     std::cout << "[2] (size) Passed" << std::endl;
-  } else {
+  }
+  else
+  {
     std::cout << "[2] (size) Failed" << std::endl;
     failed_count++;
   }
 
-  if(x.pop_back() == 123){
+  if (x.pop_back() == 123)
+  {
     std::cout << "[3] (pop_back) Passed" << std::endl;
-  } else {
+  }
+  else
+  {
     std::cout << "[3] (pop_back) Failed" << std::endl;
     failed_count++;
   }
 
-  if(x.size() == 2){
+  if (x.size() == 2)
+  {
     std::cout << "[4] (size) Passed" << std::endl;
-  } else {
+  }
+  else
+  {
     std::cout << "[4] (size) Failed" << std::endl;
     failed_count++;
   }
 
-  if(x.at(1) == 69){
+  if (x.at(1) == 69)
+  {
     std::cout << "[5] (at) Passed" << std::endl;
-  } else {
+  }
+  else
+  {
     std::cout << "[5] (at) Failed" << std::endl;
     failed_count++;
   }
 
-  if(x[1] == 69){
+  if (x[1] == 69)
+  {
     std::cout << "[6] (operator[]) Paased" << std::endl;
-  } else {
+  }
+  else
+  {
     std::cout << "[6] (operator[]) Failed" << std::endl;
     failed_count++;
   }
 
-  if(failed_count == 0){
+  if (failed_count == 0)
+  {
     std::cout << "All tests passed" << std::endl;
-  } else {
+  }
+  else
+  {
     std::cout << "Some tests failed: something may not work properly" << std::endl;
   }
 
