21093b3b67b7baef7541c95db69f4b7ad7a334f8
aceinetx <mouselin82@gmail.com>
2025-04-02 00:14:39 +0300

    Use CTarget instead of Target

--- rebuild.cpp
+++ rebuild.cpp
@@ -3,10 +3,10 @@
 int main(int argc, char **argv) {
   system("mkdir -p build");
   rebuild_targets.push_back(
-      new Target("build/linked_tests", {"build/linked_tests.o"},
-                 "g++ -o build/linked_tests build/linked_tests.o"));
-  rebuild_targets.push_back(
-      new Target("build/linked_tests.o", {"tests/tests.cpp", "linked/linked.h"},
-                 "g++ -c -o build/linked_tests.o -Ilinked tests/tests.cpp"));
+      CTarget::create("build/linked_tests", {"build/linked_tests.o"},
+                      "g++ -o build/linked_tests build/linked_tests.o"));
+  rebuild_targets.push_back(CTarget::create(
+      "build/linked_tests.o", {"tests/tests.cpp"},
+      "g++ -c -o build/linked_tests.o -Ilinked tests/tests.cpp"));
   return 0;
 }
