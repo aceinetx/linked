7c7c5364454293b555f105e9e43e6b2ab6c09ec8
aceinetx <mouselin82@gmail.com>
2025-04-02 00:17:31 +0300

    Fix rebuild & make it use #OUT and #DEPENDS

--- rebuild.cpp
+++ rebuild.cpp
@@ -2,11 +2,11 @@
 
 int main(int argc, char **argv) {
   system("mkdir -p build");
+  rebuild_targets.push_back(Target::create(
+      "build/linked_tests", {"build/linked_tests.o"}, "g++ -o #OUT #DEPENDS"));
   rebuild_targets.push_back(
-      CTarget::create("build/linked_tests", {"build/linked_tests.o"},
-                      "g++ -o build/linked_tests build/linked_tests.o"));
-  rebuild_targets.push_back(CTarget::create(
-      "build/linked_tests.o", {"tests/tests.cpp"},
-      "g++ -c -o build/linked_tests.o -Ilinked tests/tests.cpp"));
+      CTarget::create("build/linked_tests.o", {"tests/tests.cpp"},
+                      "g++ -c -o #OUT #DEPENDS -Ilinked",
+                      REBUILD_STANDARD_CXX_COMPILER, "-Ilinked"));
   return 0;
 }
